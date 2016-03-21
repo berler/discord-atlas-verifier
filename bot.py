5ae93e39169a96fcba8abecac5ea20b9e969fafc
Steven Berler <berler@gmail.com>
2016-03-20 17:07:40 -0700

    start refactor of bot
    Changes to use aiohttp instead of requests, and refactored to use
    discord.ext commands. Still need to change persistent files to use
    cupboard instead of flat txt files, and split functions into multiple
    modules.

--- bot.py
+++ bot.py
@@ -7,8 +7,10 @@ import subprocess
 import sys
 import json
 
-import requests
+import aiohttp
 import discord
+from discord.ext import commands
+import cupboard
 from bs4 import BeautifulSoup
 
 logging.basicConfig(level=logging.WARNING)
@@ -48,9 +50,28 @@ with open('verified_forum_ids.txt', 'r') as f:
 public_channel = None
 mod_channel = None
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!', pm_help=True, help_attrs={'name': 'cmdhelp'})
 
-@client.event
+@asyncio.coroutine
+def welcome(user):
+    m = config['welcome_message'].format(
+            name = user.name,
+            mention_name = user.mention,
+            id = user.id,
+            )
+    if public_channel is not None:
+        yield from bot.send_message(public_channel, m)
+
+@asyncio.coroutine
+def help_message(user):
+    m = config['help_message'].format(
+            name = user.name,
+            mention_name = user.mention,
+            id = user.id,
+            )
+    yield from bot.send_message(user, m)
+
+@bot.listen
 @asyncio.coroutine
 def on_member_join(member):
     server = member.server
@@ -60,39 +81,41 @@ def on_member_join(member):
     # debug!
     print('{} [id = {}] joined the server'.format(member.name, member.id))
 
-    # check for already being verified
-    for role in member.roles:
-        if role.id == config['verified_role']:
-            print('{} is already verified'.format(member.name))
-            verified_users.add(member.id)
-            return
-
     yield from help_message(member)
     yield from welcome(member)
 
-@client.event
+@bot.command(pass_context=True, aliases=['hello'])
 @asyncio.coroutine
-def on_message(message):
+def help(ctx):
+    """Shows instructions on how to register your account"""
+    yield from help_message(ctx.message.author)
+
+@bot.listen('on_message')
+@asyncio.coroutine
+def try_verify_user(message):
     if ignore_message(message):
         return
 
     user = message.author
     content = message.content
 
-    if content.startswith('!help') or content.startswith('!hello'):
-        yield from help_message(user)
-    elif content.startswith('!'):
-        if is_mod(user):
-            yield from mod_command(message)
-    elif not is_verified(user):
-        yield from try_verify(message)
+    if is_verified(user):
+        return
+
+    for word in content.split():
+        if word.lower().startswith(config['verify_url_prefix']):
+            ret = yield from verify_provided_link(user, word)
+            return ret
+        elif word.startswith('https://') or word.startswith('http://'):
+            yield from bot.send_message(user, config['invalid_link_message'])
+            return
 
 def ignore_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         # ignore messages from ourselves!
         return True
 
-    # TODO: add per-user rate limiting
+    # TODO: add per-user rate limiting?
 
     if message.server is None:
         # private message. never ignore
@@ -110,25 +133,6 @@ def ignore_message(message):
 
     # otherwise, ignore message
     return True
-
-@asyncio.coroutine
-def welcome(user):
-    m = config['welcome_message'].format(
-            name = user.name,
-            mention_name = user.mention,
-            id = user.id,
-            )
-    if public_channel is not None:
-        yield from client.send_message(public_channel, m)
-
-@asyncio.coroutine
-def help_message(user):
-    m = config['help_message'].format(
-            name = user.name,
-            mention_name = user.mention,
-            id = user.id,
-            )
-    yield from client.send_message(user, m)
 
 def is_verified(user):
     # TODO: we might want some fallback to query the server in case our
@@ -148,34 +152,38 @@ def is_mod(user):
 
     return False
 
+def mod_only_command():
+    return commands.check(lambda ctx: is_mod(ctx.message.author))
+
+@bot.command()
+@mod_only_command()
 @asyncio.coroutine
-def mod_command(message):
-    response = None
-    if message.content == '!about':
-        response = about()
-    elif message.content == '!stats':
-        response = stats()
-    elif message.content == '!refresh':
-        response = refresh()
-    else:
-        return
-    yield from client.send_message(message.channel, response)
-
 def about():
-    return 'python version: {}\ndiscord.py version: {}\nbot version: {}'.format(
+    """Show version info for the bot"""
+    msg = 'python version: {}\ndiscord.py version: {}\nbot version: {}'.format(
             sys.version.split()[0], discord.__version__, version)
+    yield from bot.say(msg)
 
+@bot.command()
+@mod_only_command()
+@asyncio.coroutine
 def stats():
+    """Show bot stats"""
     uptime = datetime.datetime.today() - startup_time
     # strip ugly microseconds
     nice_uptime = datetime.timedelta(uptime.days, uptime.seconds, 0)
-    return 'Bot uptime: {}\nVerified users: {}\nUnique forum profiles verified: {}'.format(
+    msg = 'Bot uptime: {}\nVerified users: {}\nUnique forum profiles verified: {}'.format(
             nice_uptime, len(verified_users), len(verified_forum_ids))
+    yield from bot.say(msg)
 
+@bot.command()
+@mod_only_command()
+@asyncio.coroutine
 def refresh():
+    """Refresh the verified users cache"""
     # refresh verified users cache
     new_verified_users = set()
-    for server in client.servers:
+    for server in bot.servers:
         if config['server'] != server.id:
             continue
 
@@ -187,54 +195,48 @@ def refresh():
     removed = len(verified_users.difference(new_verified_users))
 
     if added == 0 and removed == 0:
-        return 'Verified cache refreshed (no changes)'
+        msg = 'Verified cache refreshed (no changes)'
+        yield from bot.say(msg)
+        return
 
     # replace
     verified_users.clear()
     verified_users.update(new_verified_users)
-    return 'Verified cache refreshed ({} added, {} removed)'.format(added, removed)
+
+    msg = 'Verified cache refreshed ({} added, {} removed)'.format(added, removed)
+    yield from bot.say(msg)
 
 @asyncio.coroutine
-def try_verify(message):
-    user = message.author
-    content = message.content
-    for word in content.split():
-        if word.lower().startswith(config['verify_url_prefix']):
-            ret = yield from verify(user, word)
-            return ret
-        elif word.startswith('https://') or word.startswith('http://'):
-            yield from client.send_message(user, config['invalid_link_message'])
-            return
-
-@asyncio.coroutine
-def verify(user, link):
+def verify_provided_link(user, link):
     print('Attempting to verify user {} with link {}'.format(user.id, link))
 
     # TODO: we might want a better way than just stuffing hard-coded cookies
     # (like auto login with user and pass and get the cookies from that)
-    r = requests.get(link, cookies=config['verify_cookies'])
-    if r.status_code != requests.codes.ok:
-        print('Error loading verification page:', r.status_code)
-        yield from client.send_message(user, config['verification_error'])
-        return
+    with aiohttp.ClientSession(cookies=config['verify_cookies']) as session:
+        r = yield from session.get(link)
+        if r.status != 200:
+            print('Error loading verification page:', r.status_code)
+            yield from bot.send_message(user, config['verification_error'])
+            return
 
-    # note: apparently the 'lxml' parser is faster, but you need to install it
-    soup = BeautifulSoup(r.content, 'html.parser')
-    posts = soup.findAll('div', {'class': 'ItemContent Activity'})
-    for post in posts:
-        # TODO: verify that this post is by the correct author
-        text = ' '.join(post.findAll(text=True))
-        print('Found Post:', text)
-        if user.id in text and 'discord' in text.lower():
-            # verify success!
-            ret = yield from verify_success(user, link)
-            return ret
+        # note: apparently the 'lxml' parser is faster, but you need to install it
+        content = yield from r.text()
+        soup = BeautifulSoup(content, 'html.parser')
+        posts = soup.findAll('div', {'class': 'ItemContent Activity'})
+        for post in posts:
+            # TODO: verify that this post is by the correct author
+            text = ' '.join(post.findAll(text=True))
+            print('Found Post:', text)
+            if user.id in text and 'discord' in text.lower():
+                # verify success!
+                ret = yield from verify_success(user, link)
+                return ret
 
     print('No verification post found for user', user.id)
     msg = config['missing_verification_post'].format(
             id = user.id,
             )
-    yield from client.send_message(user, msg)
+    yield from bot.send_message(user, msg)
 
 @asyncio.coroutine
 def verify_success(user, link):
@@ -251,21 +253,21 @@ def verify_success(user, link):
     # the same name, or the forum account has been used before, we will alert
     # the mods, and not verify the user. We want to avoid having impersonators.
     if dupe_user_names(user):
-        yield from client.send_message(user, config['verified_profile_duplicate_name'])
+        yield from bot.send_message(user, config['verified_profile_duplicate_name'])
         if mod_channel is not None and user.id not in manually_verified_users:
             msg = config['verified_public_message'].format(**format_args)
             msg += config['verified_profile_duplicate_name_mods'].format(
                     name = user.name,
                     )
-            yield from client.send_message(mod_channel, msg)
+            yield from bot.send_message(mod_channel, msg)
             manually_verified_users.add(user.id)
         return
     elif forum_account_used(forum_id):
-        yield from client.send_message(user, config['verified_profile_before'])
+        yield from bot.send_message(user, config['verified_profile_before'])
         if mod_channel is not None and user.id not in manually_verified_users:
             msg = config['verified_public_message'].format(**format_args)
             msg += config['verified_profile_before_mods']
-            yield from client.send_message(mod_channel, msg)
+            yield from bot.send_message(mod_channel, msg)
             manually_verified_users.add(user.id)
         return
 
@@ -276,17 +278,17 @@ def verify_success(user, link):
     if member is None:
         # TODO: make a proper error message, this shouldn't happen
         return
-    yield from client.add_roles(member, discord.Role(id=config['verified_role']))
+    yield from bot.add_roles(member, discord.Role(id=config['verified_role']))
 
     verified_forum_ids.add(forum_id)
     with open('verified_forum_ids.txt', 'a+') as f:
         f.write(forum_id + '\n')
 
     priv_message = config['verified_private_message'].format(**format_args)
-    yield from client.send_message(user, priv_message)
+    yield from bot.send_message(user, priv_message)
     if public_channel is not None:
         pub_message = config['verified_public_message'].format(**format_args)
-        yield from client.send_message(public_channel, pub_message)
+        yield from bot.send_message(public_channel, pub_message)
 
     verified_users.add(user.id)
     print('Verified user {} successfully'.format(user.id))
@@ -300,7 +302,7 @@ def get_forum_id(link):
 
 def dupe_user_names(user):
     count = 0
-    for server in client.servers:
+    for server in bot.servers:
         if server.id != config['server']:
             continue
 
@@ -320,7 +322,7 @@ def get_member(user):
     if isinstance(user, discord.Member) and user.server.id == config['server']:
         return user
 
-    for server in client.servers:
+    for server in bot.servers:
         if server.id != config['server']:
             continue
         return server.get_member(user.id)
@@ -328,16 +330,16 @@ def get_member(user):
     # member not found
     return None
 
-@client.event
+@bot.event
 @asyncio.coroutine
 def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
     # get a list of all the users already verified so we can ignore them
-    for server in client.servers:
+    for server in bot.servers:
         if server.id != config['server']:
             continue
 
@@ -352,7 +354,7 @@ def on_ready():
     print('------')
 
     # find the channels we care about
-    for channel in client.get_all_channels():
+    for channel in bot.get_all_channels():
         if config['channel'] in [channel.id, channel.name]:
             # this is ugly, but we need to tell python we are setting
             # the global var
@@ -362,4 +364,4 @@ def on_ready():
             global mod_channel
             mod_channel = channel
 
-client.run(config['email'], config['password'])
+bot.run(config['email'], config['password'])
